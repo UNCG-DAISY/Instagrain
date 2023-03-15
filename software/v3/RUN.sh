feaa5adfdb64844deee6b5ba74248fe2f9780b54
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2023-03-14 20:28:27 -0400

    comments

--- software/v3/RUN.sh
+++ software/v3/RUN.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Change the current working directory to where the source code is located
 cd /home/sediment/Documents/src
 
+# Execute the main Python script that runs a Tkinter GUI
 python main_tk.py
